08162462ebe4c9046605fde1a52299b7c9a958d8
yr2610 <shin.nakaji@nifty.com>
2025-12-21 22:18:58 +0900

    Array.prototype.reduce削除

--- SheetRenderer/scripts/app/utilities.js
+++ SheetRenderer/scripts/app/utilities.js
@@ -1,31 +1,4 @@
-﻿if (!Array.prototype.reduce) {
-  Array.prototype.reduce = function reduce(accumulator){
-    if (this===null || this===undefined) throw new TypeError("Object is null or undefined");
-
-    var i = 0, l = this.length >> 0, curr;
-
-    if(typeof accumulator !== "function") // ES5 : "If IsCallable(callbackfn) is false, throw a TypeError exception."
-      throw new TypeError("First argument is not callable");
-
-    if(arguments.length < 2) {
-      if (l === 0) throw new TypeError("Array length is 0 and no second argument");
-      curr = this[0];
-      i = 1; // start accumulating at the second element
-    }
-    else
-      curr = arguments[1];
-
-    while (i < l) {
-      if(i in this) curr = accumulator.call(undefined, curr, this[i], i, this);
-      ++i;
-    }
-
-    return curr;
-  };
-}
-
-
-// https://gist.github.com/jcxplorer/823878
+﻿// https://gist.github.com/jcxplorer/823878
 function uuid() {
   var uuid = "", i, random;
   for (i = 0; i < 32; i++) {
