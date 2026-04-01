73a0a453f6dff5e3b064afce29f7afd6fba2e70f
yr2610 <shin.nakaji@nifty.com>
2026-04-01 20:41:12 +0900

    ファイルaddし忘れ ライブラリ更新

---
+++ SheetRenderer/GitLabShareInfo.cs
@@ -0,0 +1,6 @@
+internal sealed class GitLabShareInfo
+{
+    public string BaseUrl { get; set; }
+    public string ProjectId { get; set; }
+    public string RefName { get; set; }
+}
---
+++ SheetRenderer/SharedSheetModels.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+
+internal sealed class SharedSheetDocument
+{
+    public string Project { get; set; }
+    public string SheetId { get; set; }
+    public string SheetName { get; set; }
+    public string RangeAddress { get; set; }
+    public SharedRangeInfo RangeInfo { get; set; }
+    public object[][] Values { get; set; }
+    public string Hash { get; set; }
+}
+
+internal sealed class SharedRangeInfo
+{
+    public int? IdColumnOffset { get; set; }
+    public HashSet<int> IgnoreColumnOffsets { get; set; }
+}
+
+internal sealed class SharedProjectManifest
+{
+    public string Project { get; set; }
+    public string UpdatedAt { get; set; }
+    public List<SharedProjectManifestEntry> Sheets { get; set; }
+}
+
+internal sealed class SharedProjectManifestEntry
+{
+    public string SheetId { get; set; }
+    public string SheetName { get; set; }
+    public string Hash { get; set; }
+}
+
+internal sealed class SharedSheetSyncState
+{
+    public List<SharedSheetSyncStateEntry> Sheets { get; set; }
+}
+
+internal sealed class SharedSheetSyncStateEntry
+{
+    public string SheetId { get; set; }
+    public string BaseHash { get; set; }
+}
